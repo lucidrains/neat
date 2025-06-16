0d114eee1fcfa77aa5ce1a2860efbb4627fe4022
lucidrains <lucidrains@gmail.com>
2025-06-16 07:50:35 -0700

    aim to implement cppn from scratch by end of week

--- neat/neat.py
+++ neat/neat.py
@@ -1,4 +1,19 @@
 import jax
 
+import einx
+from einops import rearrange
+
+# classes
+
+class CPPN:
+  def __init__(self):
+    raise NotImplementedError
+
+class GeneticAlgorithm:
+  def __init__(self):
+    raise NotImplementedError
+
+# main
+
 if __name__ == '__main__':
   print(jax.__version__)
