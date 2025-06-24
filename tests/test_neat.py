17732a1a2b1c600850f184ba0b5975c01c770f64
lucidrains <lucidrains@gmail.com>
2025-06-24 17:25:33 +0000

    add a test for pop mlp

--- tests/test_neat.py
+++ tests/test_neat.py
@@ -9,3 +9,19 @@ def test_add_neuron_and_synapse():
     assert add_neuron(top_id) == 0
     assert add_neuron(top_id) == 1
     assert add_synapse(top_id, 0, 1) == 0
+
+# mlp with population dim
+
+import jax
+import jax.numpy as jnp
+
+from neat.neat import (
+    init_mlp_weights_biases,
+    mlp
+)
+
+def test_population_mlp():
+    weights, biases = init_mlp_weights_biases(10, 16, 16, 5, pop_dim = 8)
+
+    action_logits = mlp(weights, biases, jnp.zeros((8, 10)))
+    assert action_logits.shape == (8, 5)
