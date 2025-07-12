31f14d12f5865e322f833ad62658a984cfe7d044
lucidrains <lucidrains@gmail.com>
2025-07-12 11:48:13 -0700

    try again

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from nimporter import *
 setup(
     name = 'neat',
     py_modules = ['neat/neat'],
-    ext_modules = get_nim_extensions(
+    ext_modules = build_nim_extensions(
         platforms = [WINDOWS, LINUX, MACOS]
     )
 )
