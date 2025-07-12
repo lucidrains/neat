5cd921ae06c4d2c610b8834d004d5c144a0a410a
lucidrains <lucidrains@gmail.com>
2025-07-12 12:02:45 -0700

    again

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from nimporter import *
 
 setup(
     name = 'neat',
-    py_modules = ['neat/neat'],
-    ext_modules = build_nim_extensions(
-        platforms = [WINDOWS, LINUX, MACOS]
-    )
+    py_modules = ['neat'],
+    ext_modules = build_nim_extensions()
 )
