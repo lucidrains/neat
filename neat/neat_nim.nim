d15f628ad6a008aa8b3bd9f67ced2c7972a05534
lucidrains <lucidrains@gmail.com>
2025-07-18 19:36:18 -0700

    back to malebolgia, some build error on mac os with weave

--- neat/neat_nim.nim
+++ neat/neat_nim.nim
@@ -21,7 +21,7 @@ import bitvector
 
 import arraymancer
 
-import weave
+import malebolgia
 
 type
   Prob = range[0.0..1.0]
@@ -31,11 +31,6 @@ type
 randomize()
 
 # templates
-
-template with_weave(code: untyped) =
-  Weave.init()
-  code
-  Weave.exit()
 
 template benchmark(
   name: string,
@@ -804,9 +799,11 @@ proc evaluate_population(
 
   assert inputs.len == top.pop_size
 
-  # using weave for multi-threading
+  var master = create_master()
 
-  with_weave():
+  # using malebolgia for multi-threading
+
+  master.await_all:
 
     for nn_id, input in inputs:
 
@@ -827,17 +824,13 @@ proc evaluate_population(
 
       # spawn thread
 
-      spawn evaluate_nn_single_with_trace_thread_fn(
+      master.spawn evaluate_nn_single_with_trace_thread_fn(
         nn.cached_exec_trace.get.addr,
         buffer_input,
         buffer_output
       )
 
       result.add(output)
-
-    # sync all
-
-    Weave.sync_root()
 
 proc generate_hyper_weights(
   top_id: int,
